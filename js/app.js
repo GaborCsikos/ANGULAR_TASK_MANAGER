00b488e2649bc3e46825c5a31bb2333b11b1279e
Gabor Csikos <csiki123@gmail.com>
2017-05-27 13:57:43 +0200

    Added delete, and create function

--- js/app.js
+++ js/app.js
@@ -10,22 +10,95 @@ app.config(function($routeProvider){
             templateUrl: "views/addItem.html",
             controller: "TaskItemController"
         })
+         .when("/addItem/:id/",{
+            templateUrl: "views/addItem.html",
+            controller: "TaskItemController"
+        })
         .otherwise({
             redirectTo: "/"
         })
 });
 
 
-app.controller("HomeController", ["$scope", function($scope) {
-    $scope.appTitle = "TODO List";
+app.controller("HomeController", ["$scope", "TaskService", function($scope, TaskService) {
+    $scope.taskItems = TaskService.taskItems;
+ 	$scope.appTitle = "TODO List";
+    $scope.removeItem = function(entry){
+        TaskService.removeItem(entry);
+    }
 }]);
 
-app.controller("TaskItemController", ["$scope", function($scope){
+app.controller("TaskItemController", ["$scope", "$routeParams", "$location", "TaskService", function($scope, $routeParams, $location, TaskService){
 
-    $scope.taskItems = [
-        {completed: false, itemName: 'important', priority: 1},
-        {completed: false, itemName: 'medium', priority: 2},
-        {completed: false, itemName: 'not important', priority: 3},
-    ]
+     if(!$routeParams.id) {
+        $scope.taskItem = {id: 0, completed: false, itemName: ""};
+    }else{
+        $scope.taskItem = _.clone(TaskService.findById(parseInt($routeParams.id)));
+    }
+
+
+    $scope.save = function(){
+        TaskService.save( $scope.taskItem );
+        $location.path("/");
+    };
 
 }]);
+
+
+
+app.service("TaskService", function(){
+
+    var taskService = {};
+
+    taskService.taskItems = [
+        {id: 1, completed: false, itemName: 'important'},
+        {id: 2, completed: false, itemName: 'medium'},
+        {id: 3, completed: false, itemName: 'not important'}
+    ];
+
+
+        taskService.findById = function(id){
+        for(var item in taskService.taskItems){
+            if(taskService.taskItems[item].id === id) {
+                console.log(taskService.taskItems[item]);
+                return taskService.taskItems[item];
+            }
+        }
+    };
+
+    taskService.getNewId = function(){
+
+        if(taskService.newId){
+            taskService.newId++;
+            return taskService.newId;
+        }else{
+            var maxId = _.max(taskService.taskItems, function(entry){ return entry.id;})
+            taskService.newId = maxId.id + 1;
+            return taskService.newId;
+        }
+    };
+
+    taskService.removeItem = function(entry){
+        var index = taskService.taskItems.indexOf(entry);
+
+        taskService.taskItems.splice(index, 1);
+    };
+
+    taskService.save = function(entry) {
+
+        var updatedItem = taskService.findById(entry.id);
+			console.log("SAVE");
+        if(updatedItem){
+            updatedItem.completed = entry.completed;
+            updatedItem.itemName = entry.itemName;
+
+        }else {
+            entry.id = taskService.getNewId();
+            taskService.taskItems.push(entry);
+        }
+
+    };
+
+    return taskService;
+
+});
