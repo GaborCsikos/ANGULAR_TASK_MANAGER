4bd312db03e78cad03153a0d459b865bd5280428
Gabor Csikos <csiki123@gmail.com>
2017-05-27 10:46:32 +0200

    Added simple routing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-var app = angular.module('taskManagerApp', []);
+var app = angular.module('taskManagerApp', ["ngRoute"]);
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when("/",{
+            templateUrl: "views/taskList.html",
+            controller: "TaskItemController"
+        })
+        .when("/addItem",{
+            templateUrl: "views/addItem.html",
+            controller: "TaskItemController"
+        })
+        .otherwise({
+            redirectTo: "/"
+        })
+});
+
 
 app.controller("HomeController", ["$scope", function($scope) {
     $scope.appTitle = "TODO List";
@@ -7,9 +23,9 @@ app.controller("HomeController", ["$scope", function($scope) {
 app.controller("TaskItemController", ["$scope", function($scope){
 
     $scope.taskItems = [
-        {completed: false, itemName: 'important', priority: 'HIGH'},
-        {completed: false, itemName: 'medium', priority: 'MEDIUM'},
-        {completed: false, itemName: 'not important', priority: 'LOW'},
+        {completed: false, itemName: 'important', priority: 1},
+        {completed: false, itemName: 'medium', priority: 2},
+        {completed: false, itemName: 'not important', priority: 3},
     ]
 
 }]);
